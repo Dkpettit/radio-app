f560b5b75f737dacb3eac82a50086ef86db174bd
Dave <dkpettit@gmail.com>
2024-02-27 14:51:16 -0500

    removed erroneous directories

--- backend/routes/memberRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { addMember } = require('../controllers/memberController')
-const router = express.Router()
-
-router.post('/', addMember)
-
-
-module.exports = router
